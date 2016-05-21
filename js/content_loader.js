af55d91be7ffdee00514e0d1bf22a173470a9315
Yang Chen <yang2qwe@yahoo.ca>
2016-05-21 16:51:18 -0400

    Major re-vamp
    Re-design of layout and navigation structure.
    Added content.

---
+++ js/content_loader.js
@@ -0,0 +1,5 @@
+$( "#sony_content" ).load( "/project_content.html #sony_content" );
+$( "#redlab_content" ).load( "/project_content.html #redlab_content" );
+$( "#farmtable_content" ).load( "/project_content.html #farmtable_content" );
+$( "#judio_content" ).load( "/project_content.html #judio_content" );
+

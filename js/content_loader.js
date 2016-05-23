59593d7b0a0e252ddcf819e8ab8f859e99403418
Yang Chen <yang2qwe@yahoo.ca>
2016-05-23 00:54:02 -0400

    Added Card Sorting

--- js/content_loader.js
+++ js/content_loader.js
@@ -3,4 +3,4 @@ $( "#redlab_content" ).load( "/project_content.html #redlab_content" );
 $( "#farmtable_content" ).load( "/project_content.html #farmtable_content" );
 $( "#judio_content" ).load( "/project_content.html #judio_content" );
 $( "#canacc_content" ).load( "/project_content.html #canacc_content" );
-
+$( "#boltmade_cardsorting_content" ).load( "/project_content.html #boltmade_cardsorting_content" );

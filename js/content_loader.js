e83818b6ae584a3560f51f9fa263b9d6ea73ec39
Yang Chen <yang2qwe@yahoo.ca>
2017-12-08 17:44:53 -0500

    Updates to the overall template
    Reformatted landing page and nav, removed some works, added space for
    new work project

--- js/content_loader.js
+++ js/content_loader.js
@@ -1,7 +1,5 @@
+$( "#shopify_content" ).load( "/project_content.html #shopify_content" );
 $( "#pendance_content" ).load( "/project_content.html #pendance_content" );
 $( "#sony_content" ).load( "/project_content.html #sony_content" );
-$( "#redlab_content" ).load( "/project_content.html #redlab_content" );
-$( "#farmtable_content" ).load( "/project_content.html #farmtable_content" );
 $( "#judio_content" ).load( "/project_content.html #judio_content" );
-$( "#canacc_content" ).load( "/project_content.html #canacc_content" );
 $( "#boltmade_cardsorting_content" ).load( "/project_content.html #boltmade_cardsorting_content" );

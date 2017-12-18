81b98bd88e97151f08b18a23ed4b5e4cbe6b1164
Yang Chen <yang2qwe@yahoo.ca>
2017-12-18 07:12:34 -0500

    Major content updates
    Updated music page, added blog, updated styling

--- js/content_loader.js
+++ js/content_loader.js
@@ -1,4 +1,4 @@
-$( "#shopify_content" ).load( "/project_content.html #shopify_content" );
+
 $( "#pendance_content" ).load( "/project_content.html #pendance_content" );
 $( "#sony_content" ).load( "/project_content.html #sony_content" );
 $( "#judio_content" ).load( "/project_content.html #judio_content" );
